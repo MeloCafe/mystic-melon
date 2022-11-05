571fcf14205248fd81dcb6620171190a33818c88
Brian Wagner <brian.wagner35@gmail.com>
2022-11-04 22:01:39 -0700

    Serve font from Vercel

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -6,6 +6,10 @@ const Meta: React.FC = () => {
       <title>gm web3</title>
       <meta name="description" content="gm" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preload" href="/fonts/Fredoka-Regular.ttf" as="font" crossOrigin="" />
+      <link rel="preload" href="/fonts/Fredoka-Medium.ttf" as="font" crossOrigin="" />
+      <link rel="preload" href="/fonts/Fredoka-SemiBold.ttf" as="font" crossOrigin="" />
+      <link rel="preload" href="/fonts/Fredoka-Bold.ttf" as="font" crossOrigin="" />
       <meta property="og:title" content="web3 scaffold" />
       <meta property="og:description" content="gm" />
       <meta property="og:image" content="/og-image.jpg" />
