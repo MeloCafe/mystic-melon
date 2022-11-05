6ce829876c5c2686b7f8ea7c415d9edf910ca6ef
Tina Zheng <tina.s.zheng+github@gmail.com>
2022-11-04 23:10:44 -0700

    add nav bar

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled'
 import { ConnectKitButton } from 'connectkit'
 import Link from 'next/link'
 
+import { colors } from '../styles/colors'
+
 export function HeaderNav() {
   return (
     <Container>
@@ -9,7 +11,8 @@ export function HeaderNav() {
         <Title>🍈 Secret Melon</Title>
       </Left>
       <Right>
-        <Link href="/">Proposals</Link>
+        <StyledLink href="/">Proposals</StyledLink>
+        <StyledLink href="/">Votes</StyledLink>
         <ConnectKitButton theme="rounded" />
       </Right>
     </Container>
@@ -20,16 +23,43 @@ const Container = styled.div`
   display: flex;
   flex-flow: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+
+  padding: 48px;
+  color: ${colors.green400};
 `
 
 const Left = styled.div``
+
 const Right = styled.div`
   display: flex;
   align-items: center;
-  gap: 32px;
+  gap: 36px;
+  font-weight: 500;
+  font-size: 24px;
+
+  // connect kit button
+  button:hover {
+    transform: scale(1.05);
+    transition-duration: 0.2s;
+  }
+
+  button {
+    transition-duration: 0.2s;
+  }
+`
+
+const StyledLink = styled(Link)`
+  cursor: pointer;
+  padding: 8px 16px;
+
+  &:hover {
+    background-color: ${colors.green300};
+    border-radius: 20px;
+  }
 `
 
 const Title = styled.div`
   font-size: 32px;
+  font-weight: 600;
 `
