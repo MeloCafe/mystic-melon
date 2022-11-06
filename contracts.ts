0e5d9c469fb2573c23f0281ba7d1cd911bf817c7
Brian Wagner <brian.wagner35@gmail.com>
2022-11-06 08:10:35 -0800

    Add mumbai address for vault factory

--- contracts.ts
+++ contracts.ts
@@ -10,12 +10,15 @@ import { FactoryAbi, VaultAbi } from './types/ethers-contracts'
 export const VERIFIER_GOERLI = '0xdc5e5904dc9549e2a0045885145d4009c30b600a'
 export const VERIFIER_MUMBAI = '0x16c91389c6d37605e6141211707e2de1c34b4713'
 
+export const FACTORY_GOERLI = '0xB51C1ec185de4fC8a910Cb6b9bD9b2a6ce9fBC18'
+export const FACTORY_MUMBAI = '0xf54A3de5162D49098a94314eE6946807772aFA2A'
+
 export function getVaultContract(provider: Provider | Signer, address: string) {
   return new Contract(address, VaultABI, provider) as VaultAbi
 }
 
 export function getFactoryContract(signer: Signer) {
-  return new Contract('0xB51C1ec185de4fC8a910Cb6b9bD9b2a6ce9fBC18', FactoryABI, signer) as FactoryAbi
+  return new Contract(FACTORY_GOERLI, FactoryABI, signer) as FactoryAbi
 }
 
 // override wagmi's useAccount to work with NextJS SSR
